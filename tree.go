f8da58eba09b324e9ae9b2bbf463fc02d93eff27
Andrej Bencic <bencicandrej94@gmail.com>
2016-07-14 22:47:08 +0200

    Added wildcards and parameters while constructing the tree

--- tree.go
+++ tree.go
@@ -3,6 +3,8 @@ package hyper
 import (
 	"bytes"
 	"fmt"
+	"math"
+	"strings"
 )
 
 type treeNode struct {
@@ -15,16 +17,27 @@ type treeNode struct {
 // so we can easily see the current state of the
 // tree, mostly for debugging purposes.
 func (n *treeNode) String() string {
-	return n.printTree("")
+	return n.printTree(0)
 }
 
 // printTree is a utility method to pretty print
 // the tree during the String() call.
-func (n *treeNode) printTree(prefix string) string {
+func (n *treeNode) printTree(level int) string {
 	buff := &bytes.Buffer{}
-	fmt.Fprintf(buff, prefix+"%s => %v\n", n.label, n.handler != nil)
+	if level > 1 {
+		fmt.Fprint(buff, strings.Repeat("   ", int(math.Max(float64(0), float64(level-1)))))
+	}
+	if level > 0 {
+		fmt.Fprintf(buff, "└── ")
+	}
+
+    hasHandler := ""
+    if n.handler != nil {
+        hasHandler = "✓"
+    }
+	fmt.Fprintf(buff, "%s %s\n", n.label, hasHandler)
 	for _, child := range n.children {
-		fmt.Fprintf(buff, child.printTree(prefix+"\t"))
+		fmt.Fprintf(buff, child.printTree(level+1))
 	}
 	return string(buff.Bytes())
 }
@@ -87,33 +100,58 @@ func (n *treeNode) insertNode(label string, handler Handler) {
 	}
 
 	// #1 Find the common prefix size
-	prefixSize := findPrefixLength(n.label, label)
+	prefixSize := 0
 
-	// #2 If the current node is not the whole prefix, we need
-	// to split the current node into the prefix node and a child
-	// node representing the current node.
-	if len(n.label) > prefixSize {
-		n.splitNode(prefixSize)
-	}
+	switch true {
+	case n.isCatchAll():
+		//@TODO: Build full path here, for debugging purposes.
+		panic("there is a catch all route registered for label " + label)
+	case n.isParam():
+		prefixSize = findPrefixLength(n.label, label, "/")
 
-	// #3 If the
-	if len(label) == prefixSize {
-		if n.handler != nil {
+		if n.label != label[:prefixSize] {
 			//@TODO: Build full path here, for debugging purposes.
-			panic("a handler is already registered for label " + label)
+			panic("route already defined for label '" + label + "' check for " + n.label)
+		}
+		break
+	default:
+		prefixSize = findPrefixLength(n.label, label, ":*")
+
+		// #2 If the current node is not the whole prefix, we need
+		// to split the current node into the prefix node and a child
+		// node representing the current node.
+		if len(n.label) > prefixSize {
+			n.splitNode(prefixSize)
 		}
 
-		n.handler = handler
-		return
-	}
+		// #3 If the
+		if len(label) == prefixSize {
+			if n.handler != nil {
+				//@TODO: Build full path here, for debugging purposes.
+				panic("a handler is already registered for label " + label)
+			}
 
-	// #4
-	for _, child := range n.children {
-		if child.label[0] == label[prefixSize] {
-			child.insertNode(label[prefixSize:], handler)
+			n.handler = handler
 			return
 		}
 	}
+
+	// #4
+    var nextNode *treeNode
+    for _, child := range n.children {
+        if child.label[0] == byte('*') {
+            nextNode = child
+            break
+        }
+		if child.label[0] == label[prefixSize] && nextNode == nil {
+            nextNode = child
+		}
+	}
+
+    if nextNode != nil {
+        nextNode.insertNode(label[prefixSize:], handler)
+        return
+    }
 
 	// #5 There is no child that matches, create a new child.
 	newChild := treeNode{
@@ -124,11 +162,20 @@ func (n *treeNode) insertNode(label string, handler Handler) {
 	n.children = append(n.children, &newChild)
 }
 
-// findPrefixLength returns the size of the longest common prefix.
-func findPrefixLength(a, b string) int {
+func (tree treeNode) isParam() bool {
+	return len(tree.label) > 0 && tree.label[0] == ':'
+}
+
+func (tree treeNode) isCatchAll() bool {
+	return len(tree.label) > 0 && tree.label[0] == '*'
+}
+
+// findPrefixLength returns the size of the longest common prefix,
+// breaking early if encountering any of the breakAt characters.
+func findPrefixLength(a, b string, breakAt string) int {
 	i := 0
 	max := min(len(a), len(b))
-	for i < max && a[i] == b[i] {
+	for i < max && a[i] == b[i] && !strings.ContainsAny(string(a[i]), breakAt) {
 		i++
 	}
 
@@ -149,7 +196,7 @@ func min(a, b int) int {
 // splitNode splits the current node into two,
 // in order to create room for future insertions,
 // and moves the handler to the child node.
-func (n *treeNode) splitNode(splitPoint int) {
+func (n *treeNode) splitNode(splitPoint int) *treeNode {
 	child := treeNode{
 		label:    n.label[splitPoint:],
 		handler:  n.handler,
@@ -159,4 +206,6 @@ func (n *treeNode) splitNode(splitPoint int) {
 	n.label = n.label[:splitPoint]
 	n.handler = nil
 	n.children = []*treeNode{&child}
+
+	return &child
 }
--- tree_test.go
+++ tree_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"testing"
+    "fmt"
 )
 
 func testHandler(ctx context.Context, w http.ResponseWriter, r *http.Request) {
@@ -16,13 +17,20 @@ func TestTreeAddAndGet(t *testing.T) {
 	routes := [...]string{
 		"/api/v1/test",
 		"/api/v1/foo/bar",
-		"/api/v1/example",
+        "/api/v1/example",
+        "/api/v1/example/*catchAll",
+        "/api/v1/ex",
 		"/api/v1/foo",
+        "/api/v1/project/:projectId",
+        "/api/v1/project/:projectId/orion",
+        "/api/v1/query/*wild",
 	}
 
 	for _, route := range routes {
 		tree.insertNode(route, HandlerFunc(testHandler))
 	}
+
+    fmt.Print(tree.String())
 
 	tests := []struct {
 		route             string
@@ -52,10 +60,13 @@ func TestFindPrefixSize(t *testing.T) {
 		{"foo bar", "foo bar baz", 7},
 		{"foo", "Foo", 0},
 		{"foobar", "foo bar", 3},
+        {"/api/v1/users/:parameter", "/api/v1/users/:parameter", 14},
+        {"/api/v1/*rest", "/api/v1/*rest", 8},
+        {"/api/v1/users/:parameter", "/api/v1/users/something", 14},
 	}
 
 	for _, test := range tests {
-		got := findPrefixLength(test.first, test.second)
+		got := findPrefixLength(test.first, test.second, ":*")
 
 		if got != test.wanted {
 			t.Errorf(`findPrefix("%s", "%s") got "%s", wanted "%s"`, test.first, test.second, got, test.wanted)
